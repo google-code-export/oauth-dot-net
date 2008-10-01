7b541f747c9fde88437d913e671113566ea40485
chris.s.adams <chris.s.adams@abca3a7d-2553-0410-a3c2-878956a2bcef>
2008-10-01 14:47:43 +0000

    Added XRDS-Simple.Net projects for XRDS-Simple document parsing.

---
+++ trunk/XRDS-Simple.Net/Constants.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XRDS_Simple.Net
+{
+    public static class Constants
+    {
+        public const string XRDS_Version = "2.0";
+
+        public const string XRDS_Namespace = "xri://$xrds";
+
+        public const string XRD_Namespace = "xri://$XRD*($v*2.0)";
+
+        public const string XRDSimple_Namespace = "http://xrds-simple.net/core/1.0";
+
+        public const string ApplicationXRDSXML_ContentType = "application/xrds+xml";
+
+        public const string XRDSLocation_Header = "X-XRDS-Location";
+    }
+}
---
+++ trunk/XRDS-Simple.Net/DiscoveryException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XRDS_Simple.Net
+{
+    public class DiscoveryException : Exception
+    {
+        public DiscoveryException(string message) : base(message) { }
+        public DiscoveryException(string message, Exception innerException) : base(message, innerException) { }
+
+    }
+}
