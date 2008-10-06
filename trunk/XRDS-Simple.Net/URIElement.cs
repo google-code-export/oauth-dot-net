0661e242500525cd222c8d30df1256c29cf8270c
chris.s.adams <chris.s.adams@abca3a7d-2553-0410-a3c2-878956a2bcef>
2008-10-06 12:33:04 +0000

    Update comments, added copyright notice and added IPriority to URIElement and LocalIDElement

--- trunk/XRDS-Simple.Net/URIElement.cs
+++ trunk/XRDS-Simple.Net/URIElement.cs
@@ -1,4 +1,34 @@
-﻿using System;
+﻿// Copyright (c) 2008 Madgex
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+// OAuth.net uses the Windsor Container from the Castle Project. See "Castle 
+// Project License.txt" in the Licenses folder.
+//
+// XRDS-Simple.net uses the HTMLAgility Pack. See "HTML Agility Pack License.txt"
+// in the Licenses folder.
+// 
+// Authors: Chris Adams, Bruce Boughton
+// Website: http://lab.madgex.com/oauth-net/
+// Email:   oauth-dot-net@madgex.com
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +36,17 @@ using System.Xml.Serialization;
 
 namespace XRDS_Simple.Net
 {
+    /// <summary>
+    /// Representation of the a URI Element in a XRDSSimple Service definition.
+    /// </summary>
     [XmlType(Namespace = Constants.XRD_Namespace)]
     [XmlRoot(ElementName = "Service", Namespace = Constants.XRD_Namespace)]
-    public class URIElement
+    public class URIElement : IPriority
     {
+        /// <summary>
+        /// The optional priority this URI element has over other URI Elements in the same service
+        /// defintion.
+        /// </summary>
         [XmlAttribute(DataType = "nonNegativeInteger", AttributeName = "priority", Namespace = Constants.XRD_Namespace)]
         public string Priority
         {
@@ -17,6 +54,9 @@ namespace XRDS_Simple.Net
             set;
         }
 
+        /// <summary>
+        /// The HTTP method to use when accessing the service.
+        /// </summary>
         [XmlAttribute(DataType = "string", AttributeName = "httpMethod", Namespace = Constants.XRDSimple_Namespace)]
         public string HttpMethod
         {
@@ -24,6 +64,9 @@ namespace XRDS_Simple.Net
             set;
         }
 
+        /// <summary>
+        /// The textual represetation of the URI.
+        /// </summary>
         [XmlText(DataType="anyURI")]
         public string Text
         {
