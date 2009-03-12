aa540769453fffbcf9fbefd462c3ecc05b6bd347
chris.s.adams <chris.s.adams@abca3a7d-2553-0410-a3c2-878956a2bcef>
2009-03-12 12:35:14 +0000

    Release of 0.5.2.0

---
+++ tags/0.5.2.0-release/OAuth.Net.Common/Consumers/IConsumerStore.cs
@@ -0,0 +1,95 @@
+﻿// Copyright (c) 2008 Madgex
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+// OAuth.net uses the Windsor Container from the Castle Project. See "Castle 
+// Project License.txt" in the Licenses folder.
+// 
+// Authors: Bruce Boughton, Chris Adams
+// Website: http://lab.madgex.com/oauth-net/
+// Email:   oauth-dot-net@madgex.com
+
+using System.Collections.Generic;
+
+namespace OAuth.Net.Common
+{
+    public interface IConsumerStore
+    {
+        /// <summary>
+        /// Gets all the consumers in the store.
+        /// </summary>
+        ICollection<IConsumer> Consumers
+        {
+            get;
+        }
+
+        /// <summary>
+        /// The number of consumers held in the store
+        /// </summary>
+        long Count
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Adds the supplied consumer to the consumer store. If the consumer conflicts with a 
+        /// consumer already in the store, then <c>false</c> is returned.
+        /// </summary>
+        /// <param name="consumer">The consumer to store</param>
+        /// <returns><c>true</c>, iff the consumer was stored</returns>
+        bool Add(IConsumer consumer);
+
+        /// <summary>
+        /// Checks whether the store contains a consumer with the given
+        /// consumer key.
+        /// 
+        /// This will return true even if the consumer has a status other than <c>Valid</c>.
+        /// </summary>
+        /// <param name="consumerKey">The consumer key</param>
+        /// <returns><c>true</c>, iff there is such a consumer 
+        /// in the store</returns>
+        bool Contains(string consumerKey);
+
+        /// <summary>
+        /// Gets the consumer registered with the given
+        /// consumer key.
+        /// </summary>
+        /// <param name="consumerKey">The consumer key</param>
+        /// <returns>The consumer registered with the consumer key if successful, or
+        /// <c>null</c> if there is no consumer registered with the supplied key</returns>
+        IConsumer GetByKey(string consumerKey);
+
+        /// <summary>
+        /// Updates the given consumer in the store.
+        /// </summary>
+        /// <param name="consumer">The consumer to update.</param>
+        /// <returns><c>true</c>, iff the consumer was successfully updated
+        /// in the store</returns>
+        bool Update(IConsumer consumer);
+
+        /// <summary>
+        /// Removes the specified consumer from the store.
+        /// </summary>
+        /// <param name="consumer">The consumer to remove</param>
+        /// <returns><c>true</c>, iff the consumer was successfully removed
+        /// from the store. This will return <c>false</c> if the consumer 
+        /// did not exist in the store.</returns>
+        bool Remove(IConsumer consumer);
+    }
+}
---
+++ tags/0.5.2.0-release/OAuth.Net.Consumer/EventArgs/AccessTokenReceivedEventArgs.cs
@@ -0,0 +1,84 @@
+﻿// Copyright (c) 2008 Madgex
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+// OAuth.net uses the Windsor Container from the Castle Project. See "Castle 
+// Project License.txt" in the Licenses folder.
+// 
+// Authors: Bruce Boughton, Chris Adams
+// Website: http://lab.madgex.com/oauth-net/
+// Email:   oauth-dot-net@madgex.com
+
+using System;
+using System.Collections.Specialized;
+using OAuth.Net.Common;
+
+namespace OAuth.Net.Consumer
+{
+    /// <summary>
+    /// The event arguments sent when an access token is received.
+    /// </summary>
+    public class AccessTokenReceivedEventArgs
+        : EventArgs
+    {
+        /// <summary>
+        /// Creates event arguments with the corresponding request token, access token
+        /// and parameter collection.
+        /// </summary>
+        /// <param name="requestToken">Request token</param>
+        /// <param name="accessToken">Access token</param>
+        /// <param name="additionalParameters">Parameter collection</param>
+        public AccessTokenReceivedEventArgs(
+            IToken requestToken,
+            IToken accessToken,
+            NameValueCollection additionalParameters)
+        {
+            this.RequestToken = requestToken;
+            this.AccessToken = accessToken;
+            this.AdditionalParameters = additionalParameters;
+        }
+
+        /// <summary>
+        /// The request token
+        /// </summary>
+        public IToken RequestToken
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// The access token
+        /// </summary>
+        public IToken AccessToken
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Any non-OAuth parameters returned by the service provider
+        /// </summary>
+        public NameValueCollection AdditionalParameters
+        {
+            get;
+            private set;
+        }
+    }
+}
