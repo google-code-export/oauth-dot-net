dac47ed22c36eeac9b0c0bd73094f095eeee9382
chris.s.adams <chris.s.adams@abca3a7d-2553-0410-a3c2-878956a2bcef>
2009-04-29 11:52:02 +0000

    Made changes to the OAuthRequest and OAuthParameters to allow the request to support any HttpMethod - so that RESTfull web requests can be made.  Additional property of HttpMethod allows you to specify what method the request for the protected resource with additional overriden GetResource methods to allow passing a stream as the entity body.

---
+++ trunk/OAuth.Net.Common/StreamCopier.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace OAuth.Net.Common
+{
+    public static class StreamCopier
+    {
+        public static void CopyTo(Stream from, Stream to)
+        {
+            if (!from.CanRead || !to.CanWrite)
+            {
+                return;
+            }
+
+            var buffer = from.CanSeek
+               ? new byte[from.Length]
+               : new byte[DefaultStreamChunkSize];
+            int read;
+
+            while ((read = from.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                to.Write(buffer, 0, read);
+            }
+        }
+
+        private const long DefaultStreamChunkSize = 0x1000;
+    }
+
+}
